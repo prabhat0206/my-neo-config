8b70f213dbec0357bc8eebeed9822daecb87e507
Prabhat Ranjan <prabhat0206@hotmail.com>
2024-01-29 00:19:21 +0530

    change in module

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,18 +1,32 @@
 return {
-    'mrcjkb/rustaceanvim',
-    version = '^4', -- Recommended
-    ft = { 'rust' },
+    "simrat39/rust-tools.nvim",
+    ft = { "rust" },
     config = function()
-        require("rustaceanvim").setup()
-        local bufnr = vim.api.nvim_get_current_buf()
-        vim.keymap.set(
-            "n",
-            "<leader>a",
-            function()
-                vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
-                -- or vim.lsp.buf.codeAction() if you don't want grouping.
-            end,
-            { silent = true, buffer = bufnr }
-        )
+        local rt = require("rust-tools")
+        local mason_registry = require("mason-registry")
+
+        local codelldb = mason_registry.get_package("codelldb")
+        local extension_path = codelldb:get_install_path() .. "/extension/"
+        local codelldb_path = extension_path .. "adapter/codelldb"
+        local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+
+        rt.setup({
+            dap = {
+                adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+            },
+            server = {
+                capabilities = require("cmp_nvim_lsp").default_capabilities(),
+                on_attach = function(_, bufnr)
+                    vim.keymap.set("n", "<Leader>k", rt.hover_actions.hover_actions, { buffer = bufnr })
+                    vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+                end,
+            },
+            tools = {
+                hover_actions = {
+                    auto_focus = true,
+                },
+            },
+        })
     end,
 }
+
