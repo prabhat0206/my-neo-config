4d8d121aaed000c29180b58ed2f8c36e0f95280d
Prabhat Ranjan <prabhat0206@hotmail.com>
2024-02-01 13:47:59 +0530

    python env fix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,6 +19,7 @@ return {
                     "pyright",
                     "tsserver",
                     "vimls",
+                    "black",
                 }
             })
         end
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,8 +2,6 @@ return {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = function()
-       vim.o.background = "dark"
-       vim.cmd([[colorscheme gruvbox]])
     end
 }
 
